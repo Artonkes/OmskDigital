a50fd0591173e113be7646bc6d3f6ab1a40ec051
Artonkes <123367923+Artonkes@users.noreply.github.com>
2025-08-07 20:42:51 +0600

    Update host

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 app.include_router(AdminRouter)
 
 origins = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
